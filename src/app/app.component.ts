928267d6ed707de3da306407ccefe5f55b7c3d55
Stephan Knapp <Stephan Knapp>
2016-10-16 22:34:57 +0200

    vertreteranview

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CleaningDataService } from './shared/cleaning-data.service';
 import { LoginService } from './shared/login.service';
 import { User } from './shared/user';
+import './shared/rxjs-operators';
 
 //import '.shared/rxjs-operators';
 
@@ -12,18 +13,34 @@ import { User } from './shared/user';
   providers: [ CleaningDataService, LoginService ]
 })
 export class AppComponent {
+  errorMessage: string;
   title = 'Putzliste';
   putzliste : string;
   user: User;
   constructor (private loginService: LoginService,private cleaningDataService : CleaningDataService){}
-   ngOnInit() { }
+   ngOnInit() { this.user = new User("test", "test");}
   GetCleaningList(){
     this.putzliste = "Alles schmutzig!!";
     this.cleaningDataService.getCleanList()
     .subscribe()
   }
   loginUser(){
+    
     this.loginService.userlogin()
-    .subscribe(user => this.user.Username = user.Username)
+    //.subscribe(response => this.user = response)
+    //.subscribe(loginData => console.log("Hello  "+ loginData),
+    .subscribe(loginData => {for (const key of Object.keys(loginData)) {
+                                    const val = loginData[key];
+                                    if(key==="Username"){
+                                      console.log("yeaaaahhh");
+                                      return this.user.Username = loginData[key];
+                                      //console.log(this.user.Username);
+                                    }else{this.user.Username = "test"}
+                                    console.log("Der Schlüssel...."+key)
+                                    console.log("haaaaalllooo" +val);
+    // use val
+    }})
+                //error => this.errorMessage = <any>error) ;
+                //error => console.log("Fehker:" + error)) ;
   }
 }
--- src/app/shared/login.service.ts
+++ src/app/shared/login.service.ts
@@ -2,25 +2,37 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { User } from '../shared/user';
-import 'rxjs/add/operator/map';
+import '../shared/rxjs-operators';
 
 
 @Injectable()
 export class LoginService {
-  constructor(private http : Http) { }
+  constructor(private http : Http) { 
+  }
   private loginURL = 'http://localhost:8080/login';
-
   userlogin():Observable<User>{
+    
     return this.http.get(this.loginURL)
-    .map(this.loginData)   
-     
+    .map(this.loginData)  
+    .catch(this.handleError)
   }
 
   private loginData(res: Response){
     console.log(res);
     let body = res.json();
-    console.log(body[0].Username);
+    console.log(body[0])
+    let user = new User(body[0].Username, "")
     //let test = body.json();
-    return body|| {};
+    //return body[0] || {};
+    return user;
+  }
+
+  private handleError (error: any) {
+    // In a real world app, we might use a remote logging infrastructure
+    // We'd also dig deeper into the error to get a better message
+    let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg); // log to console instead
+    return Observable.throw(errMsg);
   }
 }
